55a5501cf22c4c4677bf83099ca7da0819c2e54e
inluvwithcutiecats <chayanis.chot@ku.th>
2025-08-28 21:50:43 +0700

    Add files via upload

---
+++ Pair.java
@@ -0,0 +1,10 @@
+/**
+ * TODO: ทำให้คลาสนี้เป็น Generic <K, V> ที่สามารถเก็บอ็อบเจกต์ได้ 2 ชนิด
+ */
+public class Pair {
+    // TODO: สร้างฟิลด์ private final สำหรับ key และ value
+    
+    // TODO: สร้าง Constructor ที่รับ key และ value
+
+    // TODO: สร้าง Getters สำหรับ key และ value
+}
